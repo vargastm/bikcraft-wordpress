c00bc2dda0c79901fbfb73820d54cf55d2d0616d
Tiago Martins <vargas.tiago@hotmail.com>
2020-03-04 17:24:09 -0300

    Added Custom Fields on about page

--- page-about.php
+++ page-about.php
@@ -8,19 +8,14 @@ get_header();
     <section data-anime="1200" class="about-mission container fadeInDown">
         <div class="grid-10">
             <h2 class="subtitle-internal">História, Missão e Visão</h2>
-            <p>No mundo atual, a contínua expansão de nossa atividade cumpre um papel ssencial na formulação de gestão inovadora da qual fazemos parte.</p>
-            <p>No mundo atual, a contínua expansão de nossa atividade cumpre um papel ssencial na formulação de gestão inovadora da qual fazemos parte.</p>
+            <?php the_field('mission'); ?>
         </div>
         <div class="grid-6">
             <h2 class="subtitle-internal">Valores</h2>
-            <ul>
-                <li>- Qualidade no processo com</li>
-                <li>- Fico no cliente sem perder a</li>
-                <li>- Diversão, preservando a</li>
-            </ul>
+            <?php the_field('value'); ?>
         </div>
         <div class="grid-16 team-picture">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team-bikcraft.jpg" alt="Equipe Bikcraft">
+            <img src="<?php the_field('team_picture'); ?>" alt="Equipe Bikcraft">
         </div>
     </section>
     <section class="quality container">
