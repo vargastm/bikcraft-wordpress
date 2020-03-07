76628ada3688962f81f9f129086eb03df7ab770e
Tiago Martins <vargas.tiago@hotmail.com>
2020-03-07 13:44:19 -0300

    Page SEO configured

--- header.php
+++ header.php
@@ -3,7 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php bloginfo('name'); ?></title>
+        <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
+        <meta property="og:type" content="website"/>
+		<meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
+		<meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>"/>
+		<meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/og-image.png"/>
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
         <script>document.documentElement.classList.add("js");</script>
         <?php wp_head(); ?>
